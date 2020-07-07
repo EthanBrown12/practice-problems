ae4fafddde7ad33010575d1dfd93aba076151629
Ethan Brown <ethanb18@hotmail.com>
2020-07-07 16:24:04 -0700

    Attempting to submit my first pull request for practice problems repo with the word-reformer exercise

---
+++ main.js
@@ -0,0 +1,9 @@
+
+
+function wordReformer(wordFragment1, wordFragment2) {
+  return wordFragment1+wordFragment2;
+}
+
+wordReformer("bro", "ken")
+wordReformer("manipu", "lation");
+wordReformer("excite", "ment");
